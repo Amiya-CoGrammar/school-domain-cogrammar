d76b94d42687af5b6c58f1d039865b5323db7c38
CoGrammar Code Review <reviews@cogrammar.com>
2019-09-17 12:44:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+  attr_reader :roster
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grades)
+    @roster[grades]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+  end
+end
+
+school = School.new("Bayside High School")
+
+school.add_student("Zach Morris", 9)
+
+
+puts school.roster
